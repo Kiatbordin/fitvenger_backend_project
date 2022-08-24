c6455cce8cc98eac3253f8dc1fe434d35914a303
Kiatbordin Jumratanet <kiatbordin05@gmail.com>
2022-08-24 15:15:40 +0700

    add logout session destroy

--- routes/LoginRouter.js
+++ routes/LoginRouter.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const loginController = require("../controllers/LoginController");
 
 router.post("/",loginController.checkLogin2);
+router.post("/logout", (req,res,next)=>{
+    req.session.destroy();
+    res.send("Logged out.");
+});
 
 module.exports = router;
